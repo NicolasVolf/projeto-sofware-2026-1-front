3d0f44f159e14cb6dfa4a805da02c99afab4421a
Nicolas Volf <nicolas.vfs15@gmail.com>
2026-04-08 11:02:16 -0300

    sub

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-x65bk1hkee81c774.us.auth0.com"
-      clientId="kFjkpeVhXxhTGY4zKMh4ytjHhOWmdYYc"
+      domain="dev-t66siofcybz35uvi.us.auth0.com"
+      clientId="OSr7KfvoQ1iIZuLfqTVbLQVvUhPspoEY"
       authorizationParams={{
-        audience: "https://dev-x65bk1hkee81c774.us.auth0.com/api/v2/",
+        audience: "https://dev-t66siofcybz35uvi.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
